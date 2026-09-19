9de7665c53eedbb2f96b5ed81d0bd7b1dcd8ad97
sanvi-a <sanvia222@gmail.com>
2026-09-19 15:52:36 +0530

    Added Counting Stairs using Recursion program

--- Recursion/CountingStairs_Recursion.java
+++ Recursion/CountingStairs_Recursion.java
@@ -4,7 +4,7 @@ public class CountingStairs_Recursion {
     public static int countWays(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        }
+        }  
         return countWays(n - 1) + countWays(n - 2);
     }
     public static void main(String[] args) {
