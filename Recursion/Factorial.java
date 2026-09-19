1b9bb6db3593ea35cb7cfe34c663c56ffb87e71a
sanvi-a <sanvia222@gmail.com>
2026-09-19 08:44:49 +0530

    Added Factorial Program

---
+++ Recursion/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static int Factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n * Factorial(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of "+ n + " is: "+ Factorial(n));
+    }
+}
