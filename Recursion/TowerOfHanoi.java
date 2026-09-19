2700c594c632a2e492908bc3887701d4e3e9cb6b
sanvi-a <sanvia222@gmail.com>
2026-09-19 10:31:45 +0530

    Added Tower of Hanoi program

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, char source, char helper, char destination) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+        towerOfHanoi(n - 1, source, destination, helper);
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        towerOfHanoi(n - 1, helper, source, destination);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of disks: ");
+        int n = sc.nextInt();
+        towerOfHanoi(n, 'A', 'B', 'C');
+    }
+}
